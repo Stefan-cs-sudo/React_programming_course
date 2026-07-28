7c4a8fe05260e038e7131013c5bc0886873bac96
unknown <stefanadrian.deac@ulbsibiu.ro>
2026-07-28 10:37:05 +0300

    distinction between JSX and javascript

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import MyApp from './Buton.tsx'
+import Profile from './Heidi.tsx'
+let value;
+const isLoggedIn = false;
+
+if (isLoggedIn) {
+  value = <MyApp />;
+} else {
+  value = <Profile />;
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+<div>
+  {value}
+</div>
   </StrictMode>,
 )
