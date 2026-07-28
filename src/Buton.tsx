7403e290f49a3a1326ade013e7297c79755b29e4
unknown <stefanadrian.deac@ulbsibiu.ro>
2026-07-28 14:07:59 +0300

    Shared state in the component tree

--- src/Buton.tsx
+++ src/Buton.tsx
@@ -1,25 +1,31 @@
 import { useState } from 'react';
 
-function MyButton() {
+type MyButtonProps = {
+  count: number;
+  onClick: () => void;
+};
 
-      const [count, setCount] = useState(0);
+
+export default function MyApp() {
+  const [count, setCount] = useState(0);
 
   function handleClick() {
-     setCount(count + 1);
+    setCount(count + 1);
   }
 
   return (
-    <button onClick={handleClick}>
-  Clicked {count} times
-    </button>
+    <div>
+      <h1>Counters that update together</h1>
+      <MyButton count={count} onClick={handleClick} />
+      <MyButton count={count} onClick={handleClick} />
+    </div>
   );
 }
 
-export default function MyApp() {
+function MyButton({ count, onClick }:MyButtonProps) {
   return (
-    <div>
-      <h1>Welcome to my app</h1>
-      <MyButton />
-    </div>
+    <button onClick={onClick}>
+      Clicked {count} times
+    </button>
   );
 }
