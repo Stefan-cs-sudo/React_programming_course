7638884a6d9f222741b1b917c3533264776d05d0
unknown <stefanadrian.deac@ulbsibiu.ro>
2026-08-05 15:56:21 +0300

    Panel

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 
 
-import Game from './TIC-TAC-TOE.tsx'
+
+import App from './App2_PanelExample/App'
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-   <Game />
+   <App />
   </StrictMode>,
 )
